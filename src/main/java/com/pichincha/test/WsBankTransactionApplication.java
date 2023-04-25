3be2f3bd7db0212a9a4376952e8834ee661d16aa
Paul Abelardo Cuichan Ayo <pcuichan@mag.gob.ec>
2023-04-24 20:11:22 -0500

    configuration complete

---
+++ src/main/java/com/pichincha/test/WsBankTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsBankTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsBankTransactionApplication.class, args);
+	}
+
+}
